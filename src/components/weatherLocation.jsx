7a0bd5c1edb071aca6a3c93d8b4656c26801204d
Kevob1994 <kevob1994@gmail.com>
2018-07-23 21:49:17 -0430

    Crea componente weatherLocation

---
+++ src/components/weatherLocation.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class WeatherLocation extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Weather Location</h1>
+			</div>
+		);
+	}
+}
