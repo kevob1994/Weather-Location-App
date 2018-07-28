464ff1faae61a8acd0c283eeeb7b0effc1b02f0d
Kevob1994 <kevob1994@gmail.com>
2018-07-27 19:59:17 -0430

    Importa componente weatherLocation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import WeatherLocation from './components/weatherLocation';
+import WeatherLocation from './components/WeatherLocation';
 
 class App extends Component {
 	render() {
