ef4b33a249b60f053033cafd1e7d20234ea51595
Kevob1994 <kevob1994@gmail.com>
2018-10-03 21:32:53 -0430

    Agrega un estado al componente principal

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AppBar from '@material-ui/core/AppBar'
 import Paper from '@material-ui/core/Paper';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import ForecastExtended from './components/ForecastExtended';
 
 const cities = [
 	'Caracas,ve',
@@ -24,11 +25,20 @@ const styles = {
 	},
   };
 class App extends Component {
-	
+	constructor(){
+		super();
+		this.state = {
+			city: null
+		}
+	}
 	handleSelectedLocation = city => {
 		console.log(`handleSelectedLocation ${city}`)
+		this.setState( {
+			city
+		})
 	}
 	render() {
+		let {city} = this.state
 		return (
 			<MuiThemeProvider>
 			<AppBar title="WeatherApp">
@@ -48,7 +58,12 @@ class App extends Component {
 						</Col>
 						<Col xs={12} sm={12} md={12} lg={6}>
 							<Paper elevation={24}>
-							<div className="detail"></div>
+								<div className="detail">
+									{ !city ?
+										<h1>No se ha seleccionado ciudad</h1> :
+										<ForecastExtended city={city}/>}
+									
+								</div>
 							</Paper>
 						</Col>
 					</Row>		
